e241e11ac3b5b457ab4af6b8b76aa09c75d209fd
ruhulmath08 <ruhulmath08@gmail.com>
2022-04-04 15:23:42 +0600

    Java collection framework

---
+++ java/collections/src/collections/ArrayListNote.java
@@ -0,0 +1,48 @@
+package collections;
+import java.util.*;
+
+public class ArrayListNote {
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<>();
+		
+		list.add("Arif"); // Add element in list
+		list.add("Al Amin");
+		
+		list.add(1, "Kanon"); // Add element with index wise
+		
+		System.out.println(list);
+		
+		List<String> fruit = new ArrayList<>();
+		
+		fruit.add("Apple");
+		list.addAll(fruit); // Merge list into another list
+		
+		System.out.println(list);
+		System.out.println(list.get(0)); // Get element by index
+		
+		list.remove(1); // remove value using index
+		System.out.println(list);
+		
+		list.set(2, "Hamza"); // replace or updated value
+		System.out.println(list);
+		
+		System.out.println(list.contains("Arif")); // Check value is exist(true) or not(false)
+		
+		System.out.println("List size: " + list.size()); // Size of list
+		
+		// value print by Foreach loop
+		for(String element : list) {
+			System.out.println(element);
+		}
+		
+		// value print by Iterator
+		Iterator<String> iter = list.iterator();
+		
+		while(iter.hasNext()) {
+			System.out.println(iter.next());
+		}
+	}
+
+}
+	
