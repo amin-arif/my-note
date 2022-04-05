446f51929680374d4d4277ee78276c95097c8411
ruhulmath08 <ruhulmath08@gmail.com>
2022-04-05 18:04:54 +0600

    String note add | Minor change in other file

---
+++ java/StringNote.java
@@ -0,0 +1,10 @@
+
+public class StringNote {
+    public static void main(String[] args) {
+        char[] ch = { 'a', 'r', 'i', 'f' };
+
+        String st = new String(ch);
+
+        System.out.println(st);
+    }
+}
