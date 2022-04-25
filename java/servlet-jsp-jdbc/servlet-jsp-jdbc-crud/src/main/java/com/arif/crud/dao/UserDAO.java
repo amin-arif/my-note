ff1d9c6dd87f01476e6764f663722892d7481396
arif <mdarifkanon@gmail.com>
2022-04-25 07:29:47 +0600

    Servlet CRUD operation

---
+++ java/servlet-jsp-jdbc/servlet-jsp-jdbc-crud/src/main/java/com/arif/crud/dao/UserDAO.java
@@ -0,0 +1,8 @@
+package com.arif.crud.dao;
+
+// DAO -> Database Access Object
+// It is design pattern to separate database related stuffs in different class
+// It provides CRUD database operations for the table users in database.
+public class UserDAO {
+	// src: https://www.youtube.com/watch?v=RqiuxA_OFOk
+}
