82c89e54f665913d8818ce94f5c3b90cec5972f4
amin-arif <aminarif.cs@gmail.com>
2023-03-16 10:30:16 +0600

    WebSocket initiate commit

---
+++ spring/WebSocket/websocket-chat-app/src/main/java/com/dev/websocketchatapp/WebsocketChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.dev.websocketchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketChatAppApplication.class, args);
+	}
+
+}
